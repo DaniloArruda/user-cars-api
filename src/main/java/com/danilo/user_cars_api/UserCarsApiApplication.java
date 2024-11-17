1b5f11c16c7c098c6ea90ccfa90ef187b252aef1
Danilo Arruda <darruda.silva2@gmail.com>
2024-11-17 17:14:50 -0300

    initial project structure

---
+++ src/main/java/com/danilo/user_cars_api/UserCarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.user_cars_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCarsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/user_cars_api/UserCarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.user_cars_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
